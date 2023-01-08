833f0e286b725e548c3d7363809e48f4b4e1c7fe
tcy63 <2000017814@stu.pku.edu.cn>
2023-01-09 01:38:14 +0800

    add code for training with large margin cosine loss

---
+++ contrast/train.py
@@ -0,0 +1,295 @@
+from __future__ import print_function
+
+import os
+import sys
+import argparse
+import time
+import math
+import yaml
+
+import tensorboard_logger as tb_logger
+import torch
+import torch.nn as nn
+import torch.backends.cudnn as cudnn
+from torch.optim import SGD, Adam
+from torchvision import transforms, datasets
+from torchvision.models import resnet50
+
+from util import TwoCropTransform, AverageMeter
+from util import adjust_learning_rate, warmup_learning_rate, accuracy
+from util import set_optimizer, save_model
+from util import MyImageFolder
+from networks.resnet_big import SupConResNet, LMCLResNet
+from networks.layers import MarginCosineProduct, cosine_sim
+# from losses import SupConLoss
+
+# from evaluate_lmcl import validate
+
+try:
+    import apex
+    from apex import amp, optimizers
+except ImportError:
+    pass
+
+def set_loader(config):
+    if config['dataset'] == 'cifar10':
+        mean = (0.4914, 0.4822, 0.4465)
+        std = (0.2023, 0.1994, 0.2010)
+    elif config['dataset'] == 'cifar100' or config['dataset'] == 'path':
+        mean = (0.5071, 0.4867, 0.4408)
+        std = (0.2675, 0.2565, 0.2761)
+    elif config.get('mean') is not None and config.get('std') is not None:
+        mean = eval(config['mean'])
+        std = eval(config['std'])
+    else:
+        raise ValueError('dataset not supported: {}'.format(config['dataset']))
+        
+    normalize = transforms.Normalize(mean=mean, std=std)
+    
+    train_transform = transforms.Compose([
+        transforms.RandomResizedCrop(size=config['image_size'], scale=(0.2, 1.)),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomApply([
+            transforms.ColorJitter(0.3, 0.15, 0.1, 0.1)
+        ], p=0.5),
+        transforms.RandomApply([transforms.GaussianBlur(31, 2)], p=0.5),
+        transforms.RandomGrayscale(p=0.2),
+        transforms.ToTensor(),
+        normalize,
+    ])
+    
+    val_transform = transforms.Compose([
+        transforms.Resize(size=(config['image_size'], config['image_size'])), # support batching
+        transforms.ToTensor(),
+        normalize,
+    ])
+    
+    if config['model'] == 'supcon':
+        train_transform = TwoCropTransform(train_transform)
+    
+    if config['dataset'] == 'cifar10':
+        train_dataset = datasets.CIFAR10(root=config['data_folder'],
+                                         transform=train_transform,
+                                         download=True)
+    elif config['dataset'] == 'cifar100':
+        train_dataset = datasets.CIFAR100(root=config['data_folder'],
+                                          transform=train_transform,
+                                          download=True)
+    elif config['dataset'] == 'path':
+        train_dataset = MyImageFolder(root=config['data_folder']+"/train",
+                                            transform=train_transform)
+        val_dataset = MyImageFolder(root=config['data_folder']+"/val",
+                                            transform=val_transform)
+    
+    train_sampler = None
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=config['batch_size'], shuffle=(train_sampler is None),
+        num_workers=config['num_workers'], pin_memory=True, sampler=train_sampler)
+    
+    val_loader = torch.utils.data.DataLoader(
+    val_dataset, batch_size=config['batch_size'], shuffle=False,
+    num_workers=config['num_workers'], pin_memory=True)
+
+    return train_loader, val_loader
+    
+
+
+
+def set_model(config):
+    if config['model'] == 'lmcl':
+        model = LMCLResNet(**config['model_args'])
+        loss = nn.CrossEntropyLoss()
+    elif config['model'] == 'supcon':
+        model = SupConResNet(**config['model_args'])
+        loss = SupConLoss() if config.get('loss_args') is None else SupConLoss(['loss_args'])
+        
+    if config.get('load'):
+        print('Loading pretrained model from: ', config['load'])
+        model.load_state_dict(torch.load(config['load'])['model'])
+
+    if torch.cuda.is_available():
+        model = model.cuda()
+        loss = loss.cuda()
+    return model, loss
+
+
+def set_optimizer(config, params):
+    if config['optimizer'] == 'sgd':
+        optimizer = SGD(params, **config['optimizer_args'])
+    elif config['optimizer'] == 'adam':
+        optimizer = Adam(params) if config.get('optimizer_args') is None else Adam(params, **config['optimizer_args'])
+    
+#     if config.get('milestones') is not None:
+#         lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, config['milestones'])
+#     else:
+#         lr_scheduler = None
+    return optimizer
+  
+
+
+def train(train_loader, model, criterion, optimizer, epoch):
+    """one epoch training"""
+    model.train()
+
+    batch_time = AverageMeter()
+    data_time = AverageMeter()
+    losses = AverageMeter()
+    top1 = AverageMeter()
+
+    end = time.time()
+    for idx, (images, labels) in enumerate(train_loader):
+        data_time.update(time.time() - end)
+
+        if torch.cuda.is_available():
+            images = images.cuda(non_blocking=True)
+            labels = labels.cuda(non_blocking=True)
+        bsz = labels.shape[0]
+
+#         # warm-up learning rate
+#         warmup_learning_rate(opt, epoch, idx, len(train_loader), optimizer)
+
+        # compute loss
+        output = model(images, labels)
+        loss = criterion(output, labels)
+        
+        # update metric
+        losses.update(loss.item(), bsz)
+        acc1, acc5 = accuracy(output, labels, topk=(1, 5))
+        top1.update(acc1[0], bsz)
+        
+        # optimizer
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+    return losses.avg, top1.avg
+
+
+def validate(val_loader, model, criterion, config, classifier=None):
+    """validation"""
+    model.eval()
+
+    batch_time = AverageMeter()
+    losses = AverageMeter()
+    top1 = AverageMeter()
+
+    with torch.no_grad():
+        end = time.time()
+        for idx, (images, labels) in enumerate(val_loader):
+            images = images.float().cuda()
+            labels = labels.cuda()
+            bsz = labels.shape[0]
+
+            # forward
+            if config['model'] == 'lmcl':
+                output = model.encoder(images)
+                while output.dim() > 2:
+                    output = torch.squeeze(output, dim=2)
+                output = cosine_sim((output), model.layer.weight)
+                output = model(images, labels)
+                acc1, acc5 = accuracy(output, labels, topk=(1, 5))
+                loss = criterion(output, labels)
+            
+            elif config['model'] == 'supcon':
+                output = classifier(model.encoder(images))
+                loss = criterion(output, labels)
+                
+            # update metrics
+            top1.update(acc1[0], bsz)
+            losses.update(loss.item(), bsz)
+            
+            # measure elapsed time
+            batch_time.update(time.time() - end)
+            end = time.time()
+
+    return losses.avg, top1.avg
+
+def set_save(config):
+    # set the path according to the environment
+    if not os.path.isdir('./save'):
+        os.makedirs('./save')
+    save_model_path = './save/{}_models'.format(config['model'])
+    save_logger_path = './save/{}_tensorboard'.format(config['model'])
+
+    model_name = 'model_{}_load_pt_encoder_{}_optimizer_{}_bs_{}'.format(config['model'], config['model_args']['load_pt_encoder'], config['optimizer'], config['batch_size'])
+
+    save_logger_path = os.path.join(save_logger_path, model_name)
+    if not os.path.isdir(save_logger_path):
+        os.makedirs(save_logger_path)
+
+    save_model_path = os.path.join(save_model_path, model_name)
+    if not os.path.isdir(save_model_path):
+        os.makedirs(save_model_path)
+        
+    return save_model_path, save_logger_path
+
+def main(config):
+
+    best_acc = 0
+    best_epoch = 0
+    
+    ### Dataset and DataLoader ###
+    train_loader, val_loader = set_loader(config)
+
+    ### Model and Loss ###
+    model, criterion = set_model(config)
+
+    ### Optimizer ###
+    optimizer = set_optimizer(config, model.parameters())
+
+    ### Save ###
+    save_model_path, save_logger_path = set_save(config)
+    
+    logger = tb_logger.Logger(logdir=save_logger_path, flush_secs=2)
+
+    # training routine
+    for epoch in range(1, config['epochs'] + 1):
+#         adjust_learning_rate(opt, optimizer, epoch)
+
+        # train for one epoch
+        time1 = time.time()
+        train_loss, train_acc = train(train_loader, model, criterion, optimizer, epoch)
+        time2 = time.time()
+        # eval
+        val_loss, val_acc = validate(val_loader, model, criterion, config)
+        if val_acc > best_acc:
+            best_acc = val_acc
+            best_epoch = epoch
+        
+        if epoch % config['print_freq'] == 0:
+            print('epoch {}, train time {:.2f}, train_loss {:.2f}, train_acc {:.2f}; val_loss {:.2f}, val_acc {:.2f}'.format(epoch, time2 - time1, train_loss, train_acc, val_loss, val_acc))
+
+
+        # tensorboard logger
+        logger.log_value('train loss', train_loss, epoch)
+        logger.log_value('train acc', train_acc, epoch)
+        logger.log_value('val loss', val_loss, epoch)
+        logger.log_value('val acc', val_acc, epoch)
+#         logger.log_value('learning_rate', optimizer.param_groups[0]['lr'], epoch)
+
+
+        if epoch % config['save_freq'] == 0:
+            save_file = os.path.join(
+                save_model_path, 'ckpt_epoch_{epoch}.pth'.format(epoch=epoch))
+            save_model(model, optimizer, config, epoch, save_file)
+
+    # save the last model
+    save_file = os.path.join(
+        save_model_path, 'last.pth')
+    save_model(model, optimizer, config, config['epochs'], save_file)
+    
+    print('best validation accuracy: {:.2f}, epoch: {}'.format(best_acc, best_epoch))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config')
+    args = parser.parse_args()
+    
+    config = yaml.load(open(args.config, 'r'), Loader=yaml.FullLoader)
+    
+    main(config)
---
+++ contrast/util.py
@@ -0,0 +1,170 @@
+from __future__ import print_function
+
+import math
+import numpy as np
+import torch
+import torch.optim as optim
+
+from torchvision import datasets
+from torchvision.datasets import DatasetFolder
+from torchvision.datasets.folder import default_loader, IMG_EXTENSIONS, find_classes, has_file_allowed_extension
+from typing import Any, Callable, cast, Dict, List, Optional, Tuple, Union
+import os
+
+
+
+class MyImageFolder(DatasetFolder):
+    def __init__(
+        self, root: str, loader: Callable[[str], Any] = default_loader, extensions: Optional[Tuple[str, ...]] = IMG_EXTENSIONS, transform: Optional[Callable] = None, target_transform: Optional[Callable] = None, is_valid_file: Optional[Callable[[str], bool]] = None
+    ):
+
+        super().__init__(root, loader, extensions, transform=transform, target_transform=target_transform)
+
+
+    @staticmethod
+    def make_dataset(
+    directory: str,
+    class_to_idx: Optional[Dict[str, int]] = None,
+    extensions: Optional[Union[str, Tuple[str, ...]]] = None,
+    is_valid_file: Optional[Callable[[str], bool]] = None,
+) -> List[Tuple[str, int]]:
+        """Generates a list of samples of a form (path_to_sample, class).
+
+        Because one class directory in the validation directory is empty and DatasetFolder will ignore this class by default, which will distort the class_to_idx, we override the make dataset function so that it will consider all classes. The only change is in the last line. We don't raise an error if this class doesn't contain any file.
+        """
+        
+        directory = os.path.expanduser(directory)
+
+        if class_to_idx is None:
+            _, class_to_idx = find_classes(directory)
+        elif not class_to_idx:
+            raise ValueError("'class_to_index' must have at least one entry to collect any samples.")
+
+        both_none = extensions is None and is_valid_file is None
+        both_something = extensions is not None and is_valid_file is not None
+        if both_none or both_something:
+            raise ValueError("Both extensions and is_valid_file cannot be None or not None at the same time")
+
+        if extensions is not None:
+
+            def is_valid_file(x: str) -> bool:
+                return has_file_allowed_extension(x, extensions)  # type: ignore[arg-type]
+
+        is_valid_file = cast(Callable[[str], bool], is_valid_file)
+
+        instances = []
+        available_classes = set()
+        for target_class in sorted(class_to_idx.keys()):
+            class_index = class_to_idx[target_class]
+            target_dir = os.path.join(directory, target_class)
+            if not os.path.isdir(target_dir):
+                continue
+            for root, _, fnames in sorted(os.walk(target_dir, followlinks=True)):
+                for fname in sorted(fnames):
+                    path = os.path.join(root, fname)
+                    if is_valid_file(path):
+                        item = path, class_index
+                        instances.append(item)
+
+                        if target_class not in available_classes:
+                            available_classes.add(target_class)
+
+        empty_classes = set(class_to_idx.keys()) - available_classes
+        if empty_classes:
+            msg = f"Found no valid file for the classes {', '.join(sorted(empty_classes))}. "
+            if extensions is not None:
+                msg += f"Supported extensions are: {extensions if isinstance(extensions, str) else ', '.join(extensions)}"
+                
+    #         raise FileNotFoundError(msg) # Do NOT raise an error if no valid file for one class
+
+        return instances
+
+
+
+class TwoCropTransform:
+    """Create two crops of the same image"""
+    def __init__(self, transform):
+        self.transform = transform
+
+    def __call__(self, x):
+        return [self.transform(x), self.transform(x)]
+
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
+def accuracy(output, target, topk=(1,)):
+    """Computes the accuracy over the k top predictions for the specified values of k"""
+    with torch.no_grad():
+        maxk = max(topk)
+        batch_size = target.size(0)
+
+        _, pred = output.topk(maxk, 1, True, True)
+        pred = pred.t()
+        correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+        res = []
+        for k in topk:
+            correct_k = correct[:k].contiguous().view(-1).float().sum(0, keepdim=True)
+            res.append(correct_k.mul_(100.0 / batch_size))
+        return res
+
+
+def adjust_learning_rate(args, optimizer, epoch):
+    lr = args.learning_rate
+    if args.cosine:
+        eta_min = lr * (args.lr_decay_rate ** 3)
+        lr = eta_min + (lr - eta_min) * (
+                1 + math.cos(math.pi * epoch / args.epochs)) / 2
+    else:
+        steps = np.sum(epoch > np.asarray(args.lr_decay_epochs))
+        if steps > 0:
+            lr = lr * (args.lr_decay_rate ** steps)
+
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+
+def warmup_learning_rate(args, epoch, batch_id, total_batches, optimizer):
+    if args.warm and epoch <= args.warm_epochs:
+        p = (batch_id + (epoch - 1) * total_batches) / \
+            (args.warm_epochs * total_batches)
+        lr = args.warmup_from + p * (args.warmup_to - args.warmup_from)
+
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = lr
+
+
+def set_optimizer(opt, model):
+    optimizer = optim.SGD(model.parameters(),
+                          lr=opt.learning_rate,
+                          momentum=opt.momentum,
+                          weight_decay=opt.weight_decay)
+    return optimizer
+
+
+def save_model(model, optimizer, config, epoch, save_file):
+    print('==> Saving...')
+    state = {
+        'config': config,
+        'model': model.state_dict(),
+        'optimizer': optimizer.state_dict(),
+        'epoch': epoch,
+    }
+    torch.save(state, save_file)
+    del state
